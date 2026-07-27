7faf33e273ab313eb2e2bf824e7afef8420fffcb
MuraliPrasanth <83390114+MuraliPrasanth706@users.noreply.github.com>
2026-07-27 10:56:02 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left =0;
+        let right =1;
+        let maxProfit =0;
+        while(right < prices.length){
+            if(prices[right] > prices[left]){
+                let data = prices[right] - prices[left]
+                maxProfit = Math.max(data, maxProfit);
+            }else{
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit
+    }
+}
